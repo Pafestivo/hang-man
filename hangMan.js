d10af58865401ed049d7c47c846395d5fbb8dc43
Or Rodrigez <orrodrigez1@gmail.com>
2023-01-03 20:56:58 +0200

    Handle a correct guess.

--- hangMan.js
+++ hangMan.js
@@ -1,7 +1,9 @@
+alert('Please open the console to play.')
 const wordsPool = ['sea', 'ocean', 'wonderland', 'sun', 'grass', 'bottle', 'control', 'speaker', 'surf'];
 
 let chosenWord = '';
 let hiddenWord = '';
+let attempts = 10;
 
 function startGame() {
   chosenWord = wordsPool[Math.floor(Math.random() * wordsPool.length)];
@@ -10,7 +12,30 @@ function startGame() {
     hiddenWord += '*';
   }
 
-  console.log(`Guess the word: ${hiddenWord} (${chosenWord})`);
+  announceStats();
 };
+
+function announceStats() {
+  console.log(`You have ${attempts} guesses.`)
+  console.log(`The Word is:`)
+  console.log(`${hiddenWord} (${chosenWord})`);
+  handleGuess();
+}
+
+function handleGuess() {
+  let guess = prompt('What is your guess?');
+
+  if(guess === null || !isNaN(guess) || guess.length != 1) {
+    alert('This input is invalid. Please type a single letter.')
+    handleGuess();
+  } else {
+    if(chosenWord.indexOf(guess) != -1) {
+      console.log(`Nice! '${guess}' is correct`);
+      let index = chosenWord.indexOf(guess);
+      hiddenWord = hiddenWord.substring(0, index) + guess + hiddenWord.substring(index + 1);
+      announceStats();
+    } else alert('NO!');
+  }
+}
 
 startGame();
