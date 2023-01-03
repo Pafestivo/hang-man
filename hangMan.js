8ce4fbc2c97424b3c4314238367205422c46cbc2
Or Rodrigez <orrodrigez1@gmail.com>
2023-01-03 19:32:26 +0200

    Make a start game function The function will choose a random word, hide it behind asterisks, and log a message in the console.

--- hangMan.js
+++ hangMan.js
@@ -1,1 +1,16 @@
 const wordsPool = ['sea', 'ocean', 'wonderland', 'sun', 'grass', 'bottle', 'control', 'speaker', 'surf'];
+
+let chosenWord = '';
+let hiddenWord = '';
+
+function startGame() {
+  chosenWord = wordsPool[Math.floor(Math.random() * wordsPool.length)];
+
+  for (i = 0; i < chosenWord.length; i++) {
+    hiddenWord += '*';
+  }
+
+  console.log(`Guess the word: ${hiddenWord} (${chosenWord})`);
+};
+
+startGame();
