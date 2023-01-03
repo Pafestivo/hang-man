2cf9892ce2b97dc1335be216e90a8ba52258f611
Or Rodrigez <orrodrigez1@gmail.com>
2023-01-03 21:26:39 +0200

    Add a winning case when the word is revealed.

--- hangMan.js
+++ hangMan.js
@@ -7,43 +7,51 @@ let attempts = 10;
 function startGame() {
   chosenWord = wordsPool[Math.floor(Math.random() * wordsPool.length)];
 
-  for (i = 0; i < chosenWord.length; i++) {
+  for (i = 0; i < chosenWord.length; i++) { //hide the word behind asterisks
     hiddenWord += '*';
   }
 
-  alert('Please open the console to play.');
+  alert('Please open the console before continuing[F12].');
   setTimeout(() => {announceStats();}, 100);
 };
 
 function announceStats() {
-  console.log(`You have ${attempts} guesses.`)
-  console.log(`The Word is:`)
-  console.log(`${hiddenWord} (${chosenWord})`);
-  handleGuess();
+  if(hiddenWord.indexOf('*') === -1) { //if the word is discovered
+    alert('You Win!');
+    console.log(`congratulations! You win! The word was '${chosenWord}'`);
+  } else {
+    console.log(`You have ${attempts} guesses.`)
+    console.log(`The Word is:`)
+    console.log(`${hiddenWord} (${chosenWord})`);
+    handleGuess();
+  }
 }
 
 function handleGuess() {
   let guess = prompt('What is your guess?');
 
-  if(guess === null || !isNaN(guess) || guess.length != 1) {
+  if(guess === null || !isNaN(guess) || guess.length != 1) { //if input is invalid
     alert('This input is invalid. Please type a single letter.')
     handleGuess();
-  } else {
-    if(chosenWord.indexOf(guess) != -1) {
+  } 
+  else {
+    if(chosenWord.indexOf(guess) != -1) { //if guess is correct
       console.log(`Nice! '${guess}' is correct`);
       
-      for (i = 0; i < chosenWord.length; i++) {
+      for (i = 0; i < chosenWord.length; i++) { //reveal the proper letters
         if(chosenWord[i] === guess) {
           hiddenWord = hiddenWord.substring(0, i) + guess + hiddenWord.substring(i + 1)
         }
       }
       announceStats();
-    } else {
-      if(attempts > 1) {
+    } 
+    else { //if guess is wrong
+      if(attempts > 1) { 
         console.log(`Unfortunately, ${guess} is incorrect.`)
         attempts--
         announceStats();
-      } else {
+      } 
+      else { //if no attempts left
         alert('Game Over!')
         console.log(`You lost! the word was ${chosenWord}`);
       }
